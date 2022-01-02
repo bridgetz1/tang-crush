e90fb28ece407b28222f65b3f94850e3398bcb8f
Bridget <93377182+bridgetz1@users.noreply.github.com>
2022-01-02 17:48:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
-const CONFIG = const  CONFIG  =  {
-    introTitle : 'Em à!' ,
+const  CONFIG  =  {
+    titleWeb : "Hi Bbi" ,
+    introTitle : 'Kanh Đầnnn' ,
     introDesc : Thường lệ vốn trái đất
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương` ,
-    btnIntro : 'hihi' ,
-    title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 😙' ,
+    btnIntro : '^^ HiHi ^^' ,
+    title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 🥰' ,
     desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
-    btnCó : ' Cứ là thích anh <33' ,
-    btnNo : 'Không, Anh trai à: 3' ,
-    question : 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3' ,
-    btnReply : 'Gửi cho anh <3' ,
+    btn Có : 'Thích lắm <33' ,
+    btnNo : 'Không nha: 3' ,
+    question : 'Trên thế giới hơn 7 tỉ lệ người sao bạn lại yêu mình <3' ,
+    btnReply : 'Gửi cho bạn <3' ,
     reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
-    lộn xộn : 'Anh biết mà 🥰. Yêu em nhiều 😘😘 ' ,
-    messDesc : 'Tối nay công chúa voice a nghe giọng haaaa.' ,
+    mess : 'Mình biết mà 🥰. Yêu bạn nhiều 😘😘 ' ,
+    messDesc : 'Iu thì bbi chụp a xin tấm ảnhhhhhh " ,
     btn Chấp nhận : 'Okiiiii lun <3' ,
-    messLink : 'http://fb.com/thaideeptry.off' 
-}  
+    messLink : 'https://www.instagram.com/destiny.sad_/'   
+}
